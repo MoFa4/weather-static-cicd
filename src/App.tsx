c6b5f8dee89f2e6dc3d8d0dd2baebf74e7fc706e
MdFazil4 <mdfazil469@gmail.com>
2025-11-15 13:17:08 +0530

    Remove Poppins font import
    Removed Google Fonts import for Poppins.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800;900&display=swap';
 import './App.css';
 
 interface WeatherData {
